4c17dde00eed2515cc91cf3f63c23748290e1067
Maria Kamau <maria.kamau@student.moringaschool.com>
2024-04-27 13:49:29 +0300

    "Import and use username, city, and image in About and Home components; export them as named exports from user data file"

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 function NavBar() {
+  console.log("Navbar")
   return (
     <nav>
       <a href="#home">I'm a link!</a>
     </nav>
   );
 }
+
+export default NavBar;
